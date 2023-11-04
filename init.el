ccf0c04d60f9e3e67c4c6f06aed38e93ebc71fef
Markusla <Markusla@users.noreply.github.com>
2023-11-04 19:06:08 +0100

    Update init.el
    for Windows

--- init.el
+++ init.el
@@ -1,1 +1,6 @@
+;; setzen auf UTF-8 damit in Windows auch die Config mit UTF-8 Zeichen geladen werden kann
+(setq default-buffer-file-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+
+;; Laden der Config
 (org-babel-load-file (locate-user-emacs-file "config.org"))
